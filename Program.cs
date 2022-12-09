2571fcf0641e356ce10db5a664fc265cc6cdb9cc
drgrdgrd <ersfgrdg>
2022-12-10 01:39:15 +0200

    add

--- Program.cs
+++ Program.cs
@@ -3,23 +3,38 @@ using System.Security.Principal;
 
 namespace CarsOOP3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
+            //ElectricCarData carModel1 = new ElectricCarData("Model 3", CarModel.Manufacture.Tesla, 2017, 265.8, 3000, 350, new Battery(230, "Lithium-ion"), 34);
+            //TravelInfo travelInfo = new TravelInfo(53, 200, "Model 3");
+            //Tank tank = new Tank(100, "Benzine");
+            //FuelCarData carModel2 = new FuelCarData("Focus", CarModel.Manufacture.Ford, 1990, 269, 1200, 200, new Tank(340, "Disel"), 34);
+
+            //carModel1.Recharge();
+            //carModel2.Refuel();
+
+            dynamic a = 1;
+            Console.WriteLine(a);
+            a = "A";
+            Console.WriteLine(a);
+            dynamic b = new Tank();
+            Console.WriteLine(b);
         }
     }
+
     interface ITravelable
     {
-        public double MaxTravelDistance { get; protected set; }
-        public void Refuel();
-        public TravelInfo Travel(double distance);
+        double MaxTravelDistance { get; protected set; }
+        void Recharge();
+        void Refuel();
+        TravelInfo Travel(double distance);
     }
     public abstract class CarModel
     {
         public string? Model { get; protected set; }
-        public enum Manufacture 
+        public enum Manufacture
         {
             Ford,
             Mercedes,
@@ -46,44 +61,65 @@ namespace CarsOOP3
         private Battery Battery { get => Battery; set => Battery = value; }
         public double TravelDistanceKoef { get; protected set; } = 120;
         //public double MaxTravelDistance { get { return Battery.MaxCapacity; } private set { Battery.MaxCapacity } } =  ;
-        double ITravelable.MaxTravelDistance { get { return capacity; } set { value = TravelDistanceKoef * Battery.MaxCapacity; capacity = value; }
+        public double MaxTravelDistance
+        {
+            get { return capacity; }
+            set { value = TravelDistanceKoef * Battery.MaxCapacity; capacity = value; }
         }
 
-        public ElectricCarData(string Model,/*enum Manufacturer,*/ int YearOfMake, double MaxSpeed, double Weight, double LoadCapacity, Battery battery, double TravelDistanceKoef) : base()
+        public ElectricCarData(string Model, Manufacture manufacture, int YearOfMake, double MaxSpeed, double Weight, double LoadCapacity, Battery battery, double TravelDistanceKoef) : base()
         {
             base.Model = Model;
             base.StartYearOfModel = YearOfMake;
             base.MaxSpeedKmPh = MaxSpeed;
             base.Weight = Weight;
             base.LoadCappacity = LoadCapacity;
-            this.Battery= battery;
-            this.TravelDistanceKoef = TravelDistanceKoef;    
+            this.Battery = battery;
+            this.TravelDistanceKoef = TravelDistanceKoef;
         }
-        void ITravelable.Refuel()
+        public void Recharge()
         {
             Battery.RemainingCapacity = Battery.MaxCapacity;
         }
-        public TravelInfo Travel(double distance) 
+        public void Refuel() { throw new Exception("Cannot refuel electric car!"); }
+        public TravelInfo Travel(double distance)
         {
-            GetHoursForTravel(distance);
-            //TODO if проверка, защото не разбрах условието, а тук трябва да има if(){}else{}
-            throw new ApplicationException("ImpossibleTravel!");
-            Console.WriteLine("Recharge needed..." + null);
+            double remainingCapacity = distance / TravelDistanceKoef;
+            if(distance < MaxTravelDistance)
+            {
+                if(remainingCapacity > Battery.RemainingCapacity)
+                {
+                    return new TravelInfo(GetHoursForTravel(distance), distance, Model);
+                }
+                else
+                {
+                    Console.WriteLine("Recharge needed..." + null);
+                    return new TravelInfo(GetHoursForTravel(distance), distance, Model);
+                }
+            }
+            else
+            {
+                throw new ApplicationException("ImpossibleTravel!");
+            }
         }
-    }//
+    }
     public class FuelCarData : CarModel, ITravelable
     {
         private double tankCapacity;
+        private Tank Tank { get => Tank; set => Tank = value; }
 
-        public Tank Tank { get; protected set; }
-        public double MaxTravelDistanceKoef { get; protected set; } = 10;
-        double ITravelable.MaxTravelDistance
+        //public double Tank { get; protected set; }
+        public double TravelDistanceKoef { get; protected set; } = 10;
+        public double MaxTravelDistance
         {
             get { return tankCapacity; }
-            set { value = Tank.MaxCapacity * MaxTravelDistanceKoef; 
-                tankCapacity = value; }
+            set
+            {
+                value = Tank.MaxCapacity * TravelDistanceKoef;
+                tankCapacity = value;
+            }
         }
-        public FuelCarData(string Model,/*enum Manufacturer,*/ int YearOfMake, double MaxSpeed, double Weight, double LoadCapacity, Tank tank, double TravelDistanceKoef) : base()
+        public FuelCarData(string Model, Manufacture manufacture, int YearOfMake, double MaxSpeed, double Weight, double LoadCapacity, Tank tank, double TravelDistanceKoef) : base()
         {
             base.Model = Model;
             base.StartYearOfModel = YearOfMake;
@@ -91,24 +127,46 @@ namespace CarsOOP3
             base.Weight = Weight;
             base.LoadCappacity = LoadCapacity;
             this.Tank = tank;
-            this.MaxTravelDistanceKoef = TravelDistanceKoef;
+            this.TravelDistanceKoef = TravelDistanceKoef;
         }
-        void ITravelable.Refuel()
+        public void Refuel()
         {
-            throw new NotImplementedException();
+            Tank.RemainingCapacity = Tank.MaxCapacity;
+        }
+        public void Recharge() { throw new Exception("Cannot recharge fuel car!"); }
+        public TravelInfo Travel(double distance)
+        {
+            double remainingCapacity = distance / TravelDistanceKoef;
+            if (distance < MaxTravelDistance)
+            {
+                if (remainingCapacity > Tank.RemainingCapacity)
+                {
+                    return new TravelInfo(GetHoursForTravel(distance), distance, Model);
+                }
+                else
+                {
+                    Console.WriteLine("Recharge needed..." + null);
+                    return new TravelInfo(GetHoursForTravel(distance), distance, Model);
+                }
+            }
+            else
+            {
+                throw new ApplicationException("ImpossibleTravel!");
+            }
+
         }
     }
     public class Battery
     {
         public double MaxCapacity { get; protected set; }
-        public double RemainingCapacity { get; set ; }
+        public double RemainingCapacity { get; set; }
         public string Type { get; set; }//protected set
         public int LifeCycles { get; set; } = 100000;
         public Battery()
         {
-            
+
             MaxCapacity = 50;
-            RemainingCapacity= 50;
+            RemainingCapacity = 50;
             Type = "Lithium-ion";
         }
         public Battery(double MaxCapacity, string Type)
@@ -120,8 +178,8 @@ namespace CarsOOP3
     }
     public class Tank
     {
-        public Tank MaxCapacity { get; set; }
-        public double RemainingCapacity { get; protected set; } = 10;
+        public double MaxCapacity { get; set; }
+        public double RemainingCapacity { get; set; } = 10;
         public string Type { get; protected set; }
 
         public Tank()
@@ -130,36 +188,33 @@ namespace CarsOOP3
             RemainingCapacity = 50;
             Type = "Benzine";
         }
-        public Tank(Tank MaxCapacity, string Type)
+        public Tank(double MaxCapacity, string Type)
         {
-            this.MaxCapacity = MaxCapacity;
+            RemainingCapacity = MaxCapacity;
+            //this.MaxCapacity
             this.Type = Type;
         }
     }
+
+
+
     public class TravelInfo
     {
-        public double TravelTime { get; set; }
-        public double Distance { get; protected set; }
-        private string ModelCar { get; set; }
 
-        public TravelInfo(string ModelCar, double TravelTime, double Distance)
+        public double TravelTime { get; set; }
+        public double Distance { get; protected set; }//162 трябва да се премахне, защото е default стойност
+        public string ModelCar { get; protected set; }
+
+        public TravelInfo(double TravelTime, double Distance, string ModelCar)
         {
-            this.ModelCar = ModelCar;
             this.TravelTime = TravelTime;
             this.Distance = Distance;
+            this.ModelCar = ModelCar;
         }
 
         public override string ToString()
         {
             return $"{ModelCar}, traveled {Distance} km, for {TravelTime} hours";
         }
-    }
-    enum Manufacturer
-    {
-        Ford,
-        Mercedes,
-        BMW,
-        Audi,
-        Tesla
     }
 }
